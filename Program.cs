7bda32ffa5f1535ab8ed30be46a7db1b6a069133
Ilnur Mameshev <imameshev@inbox.ru>
2025-01-05 14:26:00 +0500

    Make change#

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using ConsoleApp3.manager;
 using ConsoleApp3.domain;
 
 int[] arr = new int[2] { 2, 8 };
-MovieManager manager = new MovieManager();
+//MovieManager manager = new MovieManager();
 
 
 
